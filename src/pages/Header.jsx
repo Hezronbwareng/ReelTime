39e00047a0ead839f4de29239ee99e7525e42052
Hezron Bwareng <bwarenghezron@gmail.com>
2024-12-19 18:47:02 +0300

    initial commit

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -14,24 +14,23 @@ function Header() {
 
   return (
     <header>
-    
       <a href="/" className="logo">ReelTime</a>
 
-   
-      <div className="header-actions">
-        <Search className="search-bar" />
-        <Button icon={<ion-icon name="log-in-outline"></ion-icon>} name="Sign In" className="signIn" />
-        <div className="menu-icon" onClick={toggleMenu}>
-          <ion-icon name="menu-outline"></ion-icon>
-        </div>
-      </div>
-
-      
       <ul className={`nav ${isMenuOpen ? 'nav-open' : ''}`}>
         {navListData.map(nav => (
           <NavListItem key={nav._id} nav={nav} />
         ))}
       </ul>
+
+      <div className="header-actions">
+        <Search className="search-bar" />
+        <Button icon={<ion-icon name="log-in-outline"></ion-icon>} name="Sign In" className="signIn" />
+        
+        <div className="menu-icon" onClick={toggleMenu}>
+          <ion-icon name={isMenuOpen ? 'close-outline' : 'menu-outline'}></ion-icon>
+        </div>
+        
+      </div>
     </header>
   );
 }
