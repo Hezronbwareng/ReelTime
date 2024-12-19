64a2e0e43f0a8fb331f1b42f39af0911b18109b9
Hezron Bwareng <bwarenghezron@gmail.com>
2024-12-19 21:10:09 +0300

    initial commit

--- src/components/PlayBtn.jsx
+++ src/components/PlayBtn.jsx
@@ -1,28 +1,31 @@
-import './playBtn.css'
+import './playBtn.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay } from '@fortawesome/free-solid-svg-icons';
 import Modal from './Modal';
-import { useState } from 'react'
-function PlayBtn({movie}) {
-  const [modal, setModal] = useState(false)
+import { useState } from 'react';
+
+function PlayBtn({ movie }) {
+  const [modal, setModal] = useState(false);
 
   const toggleModal = () => {
     setModal(!modal);
-  }
+  };
+
   return (
-  <>
-    <div className={`trailer d-flex align-items-center justify-content-center ${movie.active ? 'active' : undefined}`}>
-    <a href="#" className="playBtn" onClick={toggleModal}>
-        <FontAwesomeIcon icon={faPlay} size="2x" className='icon' />
-    </a>
-    <p>Watch Trailer</p>
-</div>
-{
-  movie.active && <Modal movie={movie} status ={modal} toggleModal={toggleModal}/>
+    <>
+      <div
+        className={`trailer d-flex align-items-center justify-content-center ${
+          movie.active ? 'active' : undefined
+        }`}
+      >
+        <a href="#" className="playBtn" onClick={toggleModal}>
+          <FontAwesomeIcon icon={faPlay} size="2x" className="icon" />
+        </a>
+        <p>Watch Trailer</p>
+      </div>
+      {movie.active && <Modal movie={movie} status={modal} toggleModal={toggleModal} />}
+    </>
+  );
 }
 
-  </>
-  )
-}
-
-export default PlayBtn
+export default PlayBtn;
